46c67900e129a2ee0ea532ce812314276aa44aa8
turambar2887 <turambar2887@163.com>
2025-04-10 22:09:35 +0800

    Add files via upload

---
+++ Ai Bill Application/src/test/java/Service/AIserviceTest.java
@@ -0,0 +1,23 @@
+package Service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import Service.AIservice.AITransactionService;
+
+public class AIserviceTest {
+
+    @Test
+    public void testAnalyzeTransactions() {
+        AITransactionService service = new AITransactionService();
+
+        String userRequest = "请帮我分析这个月的收支情况";
+        String filePath = "src/test/resources/sample_transactions.csv";
+
+        String result = service.analyzeTransactions(userRequest, filePath,"2025/03/20","");
+
+        assertNotNull(result, "AI分析结果不能为空");
+        System.out.println("AI分析结果: " + result);
+    }
+
+
+}
---
+++ Ai Bill Application/src/test/java/Service/AiFunctionTest.java
@@ -0,0 +1,35 @@
+package Service;
+
+import DAO.CsvTransactionDao;
+import model.Transaction;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import Service.AIservice.AITransactionService;
+
+import java.io.IOException;
+import java.util.List;
+
+public class AiFunctionTest {
+
+    @Test
+    public void testAnalyzeTransactions() throws IOException {
+
+        String filePath = "src/test/resources/sample_transactions.csv";
+        CsvTransactionDao transactionDao = new CsvTransactionDao();
+
+        List<Transaction> transactions = transactionDao.loadFromCSV(filePath);
+
+        // 实例化服务类
+        AITransactionService service = new AITransactionService();
+
+        // 调用合并格式化函数
+        List<String> result = service.formatTransactions(transactions,"2025/03/29","2025/04/10");
+
+        // 输出结果
+        result.forEach(System.out::println);
+    }
+
+
+
+
+}
